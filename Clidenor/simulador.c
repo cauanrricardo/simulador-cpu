d3478c57ebef05f785663b73ed3581dc3ffe494d
Clidenor <clidenorlopesmartinsfilho@gmail.com>
2025-02-05 19:46:42 -0300

    Atualizei as funções de  instruções

--- Clidenor/simulador.c
+++ Clidenor/simulador.c
@@ -3,48 +3,51 @@
 #include <stdint.h>
 #define memory_size 65536;
 int main(){
-    //mov
-    void mov_imediato(uint16_t *registrador, uint16_t imediato){
-        *registrador = valor_imediato;
+    //MOV
+    void movImediato(uint16_t *registrador, uint16_t imediato){
+        Resgistradores[*registrador] = imediato;
     }
-    void mov_registrador(uint16_t r_destino, uint16_t r_origem){
-        r_destino = r_origem;
+
+    void movRegistrador(uint16_t r_destino, uint16_t r_origem){
+        Registradores[r_destino] = r_origem;
     }
-    //operações
+
+
+    //ULA
     void add(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem + r_origem2;
+        Registradores[r_destino] = r_origem + r_origem2;
     }
 
     void sub(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem - r_origem2;
+       Registradores[r_destino] = r_origem - r_origem2;
     }
     
     void mul(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem * r_origem2;
+        Registradores[r_destino] = r_origem * r_origem2;
     }
 
     void div(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem / r_origem2;
+        Registradores[r_destino] = r_origem / r_origem2;
     }
 
     void and(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem & r_origem2;
+        Registradores[r_destino] = r_origem & r_origem2;
     }
 
     void xor(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem ^ r_origem2;
+        Registradores[r_destino] = r_origem ^ r_origem2;
     }
 
     void orr(uint16_t r_destino, uint16_t r_origem, uint16_t r_origem2){
-        r_destino = r_origem | r_origem2;
+        Registradores[r_destino] = r_origem | r_origem2;
     }
 
     void shr(uint16_t r_destino, uint16_t r_origem, uint16_t imediato){
-        r_destino = r_origem >> r_origem2;
+        Registradores[r_destino] = r_origem >> r_origem2;
     }
 
     void shl(uint16_t r_destino, uint16_t r_origem, uint16_t imediato){
-        r_destino = r_origem << imediato;
+       Registradores[r_destino] = r_origem << imediato;
     }
 
     uint16_t ror(uint16_t num, uint16_t n_bits){
@@ -61,27 +64,36 @@ int main(){
     
 
 
-    //pilha
-
-    //rever PILHA ESTÁ ERRADO
+    //PILHA
     void psh(uint16_t valor_registrador){
-        *SP = valor;
-        SP = SP - 1;
+        if(SP == 0){
+            printf("PILHA CHEIA!!!!!!!!!!!!, NÃO CONSIGO DAR PUSH :(\n");
+            return;
+        }
+        SP--;
+        Memoria_de_dados[SP] = Registradores[valor_registrador];
     }
-    void pop(){
+
+    //n sei se pop ta certo :/****
+    uint16_t pop(uint16_t valor_registrador){
+        if(SP == TAMANHO_DA_MEMORIA){
+            printf("PILHA VAZIA!!!!!!!!!, NÃO CONSIGO DAR POP :( \n)");
+            return;
+        }
+        uint16_t valor = Memoria_de_dados[SP];
         SP++;
-        valor = *SP;
+        return valor;
     }
 
 
 
-    //Store
+    //STORE
     void str_imediato(uint16_t *r_destino, uint16_t imediato){
-        *r_destino = imediato;
+        Registradores[*r_destino] = imediato;
     }
     
     void str_registrador(uint16_t *r_destino, uint16_t r_origem){
-        *r_destino = r_origem;
+        Registradores[*r_destino] = r_origem;
     }
 
     //NOP
@@ -94,7 +106,12 @@ int main(){
     //HALT
     void halt(){
         printf("parando programa");
-        _asm_("hlt");
+        return 0;
+    }
+
+    //LOAD
+    void ldr(uint16_t r_destino, uint16_t *r_origem){
+        Registradores[r_destino] = *r_origem;
     }
 
 
